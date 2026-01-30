e8c2798b3cc4f33190f888fc612c2847ca8fbcc7
github895439 <gl176705@gmail.com>
2026-01-31 07:14:55 +0900

    更新

--- background.js
+++ background.js
@@ -1,17 +1,18 @@
 const menus = [
-  "twitterで検索",
-  "YouTubeで検索",
+  ["twitterで検索", "selection"],
+  ["YouTubeで検索", "selection"],
+  ["twitterでtweetを検索", "link"],
 ];
 
 chrome.runtime.onInstalled.addListener(handlerInstalled);
 
 function handlerInstalled() {
   for (let index = 0; index < menus.length; index++) {
-    let title = menus[index];
+    [title, context] = menus[index];
     chrome.contextMenus.create({
       id: `menu_${index}`,
       title: title,
-      contexts: ['selection']
+      contexts: [context]
     });
   }
 
@@ -23,16 +24,25 @@ chrome.contextMenus.onClicked.addListener(handlerClick);
 function handlerClick(item, tab) {
   switch (item.menuItemId) {
     case 'menu_0': {
-      const tld = item.menuItemId;
       const url = new URL(`https://x.com/search?src=typed_query&f=live`);
       url.searchParams.set('q', `"${item.selectionText}"`);
       chrome.tabs.create({ url: url.href, index: tab.index + 1 });
       break;
     }
     case 'menu_1': {
-      const tld = item.menuItemId;
       const url = new URL(`https://www.youtube.com/results`);
       url.searchParams.set('search_query', `"${item.selectionText}"`);
+      chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+      break;
+    }
+    case 'menu_2': {
+      if (!item.linkUrl.startsWith("https://x.com/")) {
+        break;
+      }
+
+      let tmp = item.linkUrl.split("/");
+      const url = new URL(`https://x.com/search?src=typed_query&f=live`);
+      url.searchParams.set('q', `from:${tmp[3]}`);
       chrome.tabs.create({ url: url.href, index: tab.index + 1 });
       break;
     }
